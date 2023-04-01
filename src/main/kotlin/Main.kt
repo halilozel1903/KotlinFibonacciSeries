5681a19e9f635c66733556d2f088ad9c0742d2aa
Halil Özel <halilozel1903@gmail.com>
2023-04-02 02:54:05 +0300

    Fibonacci Series sample added.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val range = 10 // specify the number of terms
+    var firstNumber = 0
+    var secondNumber = 1
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    print("Fibonacci Series up to $range terms: ")
+
+    for (index in 1..range) {
+        print("$firstNumber - ".removeSuffix("-"))
+
+        val total = firstNumber + secondNumber
+        firstNumber = secondNumber
+        secondNumber = total
+    }
+
+
 }
+
